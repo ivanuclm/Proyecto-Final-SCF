aabc4c2561c2b6e7236047d9d894eb8a285e1d51
antalaveucm <162040505+antalaveucm@users.noreply.github.com>
2025-01-24 21:25:04 +0100

    Versión 1.2

--- sketch_nodo_piscina/sketch_nodo_piscina.ino
+++ sketch_nodo_piscina/sketch_nodo_piscina.ino
@@ -3,10 +3,19 @@
 #include <PubSubClient.h>
 #include <ESP32Servo.h>
 #include "DHTesp.h"
+#include <ArduinoJson.h>
+#include <NewPing.h>
+
 
 // Pines y configuración
 #define DHT_PIN 15
+#define POTENTIOMETER_PIN 13
 const int servoPin = 18;
+int potValue = 0;
+const int UltrasonicPin = 5;
+const int MaxDistance = 200;
+
+NewPing sonar(UltrasonicPin, UltrasonicPin, MaxDistance);
 
 DHTesp dhtSensor;
 Servo servo;
@@ -14,8 +23,8 @@ Servo servo;
 const char* ssid = "MiFibra-4B1E";
 const char* password = "QF6oM3q7";
 
-#define TOPIC_TEMP "/sensors/water_temperature"
-#define TOPIC_HUM "/sensors/water_level"
+#define TOPIC_DHT "/sensors/dht11"
+#define TOPIC_US "/sensors/ultrasonic"
 
 // Configuración del servidor MQTT
 const char* mqtt_server = "192.168.1.19";
@@ -55,8 +64,8 @@ void mqttConnect() {
     Serial.println("Connecting to MQTT...");
     if (client.connect("ESP32_Client", mqtt_user, mqtt_password)) {
       Serial.println("Connected to MQTT!");
-      client.subscribe(TOPIC_TEMP);
-      client.subscribe(TOPIC_HUM);
+      client.subscribe(TOPIC_DHT);
+      client.subscribe(TOPIC_US);
     } else {
       Serial.print("MQTT connection failed, state: ");
       Serial.println(client.state());
@@ -71,26 +80,25 @@ void taskDHT(void* parameter) {
     TempAndHumidity data = dhtSensor.getTempAndHumidity();
 
     if (!isnan(data.temperature) && !isnan(data.humidity)) {
-      // Convertir los valores a cadenas
-      char tempString[8];
-      char humString[8];
-      dtostrf(data.temperature, 1, 2, tempString);
-      dtostrf(data.humidity, 1, 2, humString);
+      // Crear JSON
+      StaticJsonDocument<128> jsonDoc;
+      jsonDoc["value"] = data.temperature;
+      
 
-      // Publicar los datos en MQTT
-      client.publish(TOPIC_TEMP, tempString);
-      client.publish(TOPIC_HUM, humString);
+      // Serializar JSON
+      char jsonBuffer[128];
+      serializeJson(jsonDoc, jsonBuffer);
 
-      // Mostrar en el monitor serie
-      Serial.print("Temperature: ");
-      Serial.println(tempString);
-      Serial.print("Humidity: ");
-      Serial.println(humString);
+      // Publicar en MQTT
+      client.publish(TOPIC_DHT, jsonBuffer);
+
+      // Mostrar en Monitor Serie
+      Serial.println("JSON Sent:");
+      Serial.println(jsonBuffer);
     } else {
       Serial.println("Error reading DHT sensor.");
     }
 
-    // Delay para evitar saturar el broker MQTT
     vTaskDelay(3000 / portTICK_PERIOD_MS);
   }
 }
@@ -114,6 +122,25 @@ void taskServo(void* parameter) {
   }
 }
 
+// Tarea para leer datos del potenciometro que simula el ph
+void taskPotenph(void* parameter) {
+  while (true) {
+    int potValue = analogRead(POTENTIOMETER_PIN);
+    float voltage = (potValue / 4095.0) * 3.3;
+    int percentage = map(potValue, 0, 4095, 0, 100);
+
+    Serial.print("Raw Value: ");
+    Serial.print(potValue);
+    Serial.print(" | Voltage: ");
+    Serial.print(voltage);
+    Serial.print(" V | Percentage: ");
+    Serial.print(percentage);
+    Serial.println(" %");
+
+    vTaskDelay(3000 / portTICK_PERIOD_MS);  // Leer cada 500ms
+  }
+}
+
 // Tarea para mantener la conexión WiFi y MQTT
 void taskWiFiMQTT(void* parameter) {
   while (true) {
@@ -132,12 +159,42 @@ void taskWiFiMQTT(void* parameter) {
   }
 }
 
+void taskUltrasonido(void* parameter) {  
+  while(true) {
+    float cm = sonar.ping_cm();
+    Serial.print(cm); // obtener el valor en cm (0 = fuera de rango)
+    Serial.println("cm");
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+    if (!isnan(cm)) {
+      // Crear JSON
+      StaticJsonDocument<128> jsonDoc;
+      jsonDoc["value"] = cm;
+      
+
+      // Serializar JSON
+      char jsonBuffer[128];
+      serializeJson(jsonDoc, jsonBuffer);
+
+      // Publicar en MQTT
+      client.publish(TOPIC_US, jsonBuffer);
+
+      // Mostrar en Monitor Serie
+      Serial.println("JSON Sent:");
+      Serial.println(jsonBuffer);
+    } else {
+      Serial.println("Error reading US sensor.");
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
 
   // Configurar sensor DHT y servo
   dhtSensor.setup(DHT_PIN, DHTesp::DHT11);
   servo.attach(servoPin, 500, 2400);
+  pinMode(POTENTIOMETER_PIN, INPUT);
 
   // Conexión inicial a WiFi y MQTT
   wifiConnect();
@@ -147,8 +204,10 @@ void setup() {
   xTaskCreate(taskDHT, "Task DHT", 4096, NULL, 1, NULL);
   xTaskCreate(taskServo, "Task Servo", 4096, NULL, 1, NULL);
   xTaskCreate(taskWiFiMQTT, "Task WiFi/MQTT", 4096, NULL, 1, NULL);
+  xTaskCreate(taskPotenph, "Task Potentiometer ph", 2048, NULL, 1, NULL);
+  xTaskCreate(taskUltrasonido, "Task Distancia Agua", 4096, NULL, 1, NULL);
 }
 
 void loop() {
-  // El loop queda vacío, todo se maneja con FreeRTOS
+  
 }
